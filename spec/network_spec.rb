92a83e96c502353ba60bbc978c5700f79932529b
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-26 09:56:41 -0600

    Build outputs highest paid doctor test

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -6,9 +6,11 @@ require './lib/network'
 describe Network do
   before (:each) do
     @gsmn = Network.new("Greater Seattle Medical Network")
+
     @meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
     @alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
     @seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [@meredith, @alex])
+
     @miranda = Doctor.new({name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University", salary: 150_000})
     @derek = Doctor.new({name: "Derek Sheperd", specialty: "Neurosurgery", education: "University of Pennsylvania", salary: 125_000})
     @grey_sloan = Hospital.new("Grey Sloan Memorial", "Larry Maxwell", [@miranda, @derek])
@@ -28,5 +30,12 @@ describe Network do
     @gsmn.add_hospital(@grey_sloan)
 
     expect(@gsmn.hospitals).to eq([@seattle_grace, @grey_sloan])
+  end
+
+  it 'outputs highest paid doctor' do
+    @gsmn.add_hospital(@seattle_grace)
+    @gsmn.add_hospital(@grey_sloan)
+
+    expect(@gsmn.highest_paid_doctor).to eq(@miranda)
   end
 end
