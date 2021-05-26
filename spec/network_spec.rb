83286812ec026cfdc1614aba3026d72c1a2c5d50
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-26 09:51:26 -0600

    Build test for add hospitals

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -6,6 +6,12 @@ require './lib/network'
 describe Network do
   before (:each) do
     @gsmn = Network.new("Greater Seattle Medical Network")
+    @meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+    @alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+    @seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [@meredith, @alex])
+    @miranda = Doctor.new({name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University", salary: 150_000})
+    @derek = Doctor.new({name: "Derek Sheperd", specialty: "Neurosurgery", education: "University of Pennsylvania", salary: 125_000})
+    @grey_sloan = Hospital.new("Grey Sloan Memorial", "Larry Maxwell", [@miranda, @derek])
   end
 
   it 'exists' do
@@ -15,5 +21,9 @@ describe Network do
   it 'attributes' do
     expect(@gsmn.name).to eq("Greater Seattle Medical Network")
     expect(@gsmn.hospitals).to eq([])
+  end
+
+  it 'adds hospitals' do
+    expect(@gsmn.hospitals).to eq([@seattle_grace, @grey_sloan])
   end
 end
