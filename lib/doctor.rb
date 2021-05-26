9dc02a8c2e32fed5b7bc3388d6971db05f740ff5
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-26 09:16:41 -0600

    Build methods to output doctor attributes

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -4,7 +4,19 @@ class Doctor
     @info = info
   end
 
-  # def name
-  #   @info[name]
-  # end
+  def name
+    @info[:name]
+  end
+
+  def specialty
+    @info[:specialty]
+  end
+
+  def education
+    @info[:education]
+  end
+
+  def salary
+    @info[:salary]
+  end
 end
