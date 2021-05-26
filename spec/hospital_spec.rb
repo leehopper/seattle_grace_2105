a2c2941de9086f9b0ac2bdb731e6642a762dd597
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-26 09:24:24 -0600

    Build tests for attributes

--- spec/hospital_spec.rb
+++ spec/hospital_spec.rb
@@ -4,12 +4,18 @@ require './lib/hospital'
 
 describe Hospital do
   before (:each) do
-    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
-    alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
-    seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+    @meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+    @alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+    @seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [@meredith, @alex])
   end
 
   it 'exits' do
-    expect(seattle_grace).to be_a(Hospital)
+    expect(@seattle_grace).to be_a(Hospital)
+  end
+
+  it 'has attributes' do
+    expect(@seattle_grace.name).to eq("Seattle Grace")
+    expect(@seattle_grace.chief_of_surgery).to eq("Richard Webber")
+    expect(@seattle_grace.doctors).to eq([@meredith, @alex])
   end
 end
