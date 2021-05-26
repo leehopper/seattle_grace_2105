37e2f0ddd2a81eb58eb01495d1d5c5ae45b0868f
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-26 10:58:13 -0600

    Build doctors average salary method

--- lib/network.rb
+++ lib/network.rb
@@ -27,12 +27,16 @@ class Network
 
   def doctors_by_specialty
     doctors = @hospitals.flat_map { |hospital| hospital.doctors }
-
     specialties = doctors.group_by { |doctor| doctor.specialty}
-
     output = specialties.each do |specialty, doctor|
       specialties[specialty] = doctor.map { |doctor| doctor.name }
     end
     output
+  end
+
+  def average_doctors_salary
+    doctors = @hospitals.flat_map { |hospital| hospital.doctors }
+    salaries = doctors.map { |doctor| doctor.salary}
+    average = (salaries.sum / salaries.length).to_f.round(2)
   end
 end
