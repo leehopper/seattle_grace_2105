4960fd798733caf915069603923718fe32052f11
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-26 10:22:31 -0600

    Build doctors by hospital method

--- lib/network.rb
+++ lib/network.rb
@@ -15,4 +15,13 @@ class Network
     doctors = @hospitals.flat_map { |hospital| hospital.doctors }
     doctors.max_by { |doctor| doctor.salary }
   end
+
+  def doctors_by_hospital
+    doctors = Hash.new
+    @hospitals.each do |hospital|
+      names = hospital.doctors.map { |doctor| doctor.name}
+      doctors[hospital] = names
+    end
+    doctors
+  end
 end
