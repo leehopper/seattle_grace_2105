045354fc0146bd9d2d0fb70846e8a7bc9f34021c
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-26 09:16:23 -0600

    Build tests for attributes of doctor class

--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -7,4 +7,13 @@ describe Doctor do
 
     expect(meredith).to be_a(Doctor)
   end
+
+  it 'attributes' do
+    meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+
+    expect(meredith.name).to eq("Meredith Grey")
+    expect(meredith.specialty).to eq("General Surgery")
+    expect(meredith.education).to eq("Harvard University")
+    expect(meredith.salary).to eq(100_000)
+  end
 end
