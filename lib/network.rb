272935627fbe79e14e0956afe1da5a752d7cac48
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-26 10:07:14 -0600

    Build highest paid doctor method

--- lib/network.rb
+++ lib/network.rb
@@ -10,4 +10,9 @@ class Network
   def add_hospital(hospital)
     @hospitals << hospital
   end
+
+  def highest_paid_doctor
+    doctors = @hospitals.flat_map { |hospital| hospital.doctors }
+    doctors.max_by { |doctor| doctor.salary }
+  end
 end
